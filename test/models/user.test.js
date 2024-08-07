c40c34b350d98bd5faadd4bdd8f417a2ce118f95
Akira Shirase <miguel.silva.422@gmail.com>
2024-08-08 00:56:43 +0100

    tests for missing password and invalid role

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -22,7 +22,7 @@ class MockUser {
     }
 
     async save() {
-        if (this.username === 'test' ) {
+        if (this.username === 'taken' ) {
             throw new Error('Username is already taken');
         }
 
@@ -43,32 +43,72 @@ class UserRepositoryMongoose extends UserRepositoryInterface {
         if (!userData.username) {
             throw new Error('Username is required');
         }
+
+        if (!userData.password) {
+            throw new Error('Password is required');
+        }
+
+        if (!roles.includes(userData.role)) {
+            throw new Error('Invalid role');
+        }
+
+        return new User(userData).save();
     }
 }
 
 
 describe('User Repository', () => {
-    it('Should return an error if no username is provided', async () => {
-        try {
-            await UserRepositoryMongoose.create({
-                password: 'test',
-                role: 'agent'
-            });
-        } catch (error) {
-            expect(error.message).to.equal('Username is required');
-        }
+
+    describe('Create Method: username field errors', () => {
+        it('Should return an error if no username is provided', async () => {
+            try {
+                await UserRepositoryMongoose.create({
+                    password: 'test',
+                    role: 'agent'
+                });
+            } catch (error) {
+                expect(error.message).to.equal('Username is required');
+            }
+        });
+
+        it('Should return an error if the username is already taken', async () => {
+            try {
+                await new User({
+                    username: 'taken',
+                    password: 'test',
+                    role: 'agent'
+                }).save();
+            } catch (error) {
+                expect(error.message).to.equal('Username is already taken');
+            }
+        });
     });
 
-    it('Should return an error if the username is already taken', async () => {
-        try {
-            await new User({
-                username: 'test',
-                password: 'test',
-                role: 'agent'
-            }).save();
-        } catch (error) {
-            expect(error.message).to.equal('Username is already taken');
-        }
+    describe('Create Method: password field errors', () => {
+        //Pasword field
+        it('Should return an error if no password is provided', async () => {
+            try {
+                await UserRepositoryMongoose.create({
+                    username: 'test',
+                    role: 'agent'
+                });
+            } catch (error) {
+                expect(error.message).to.equal('Password is required');
+            }
+        });
     });
 
+    describe('Create Method: role field errors', () => {
+        it('Should return an error if the role is not valid', async () => {
+            try {
+                await UserRepositoryMongoose.create({
+                    username: 'test',
+                    password: 'test',
+                    role: 'invalid'
+                });
+            } catch (error) {
+                expect(error.message).to.equal('Invalid role');
+            }
+        });
+    });
 });
